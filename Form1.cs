4307b015a168f232409d6aebea22073b4d80a76b
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2023-09-24 14:43:36 -0500

    input updates

--- Form1.cs
+++ Form1.cs
@@ -191,9 +191,9 @@ namespace InputToTxt
                     button1.Enabled = true;
                     button2.Enabled = true;
                     //comboBox1.Enabled = false;
-                    comboBox2.Enabled = false;
+                    //comboBox2.Enabled = false;
                     //comboBox3.Enabled = false;
-                    //comboBox4.Enabled = false;
+                    comboBox4.Enabled = false;
 
 
                     System.Diagnostics.Debug.WriteLine("Reading File From Device");  // Print to debug output
